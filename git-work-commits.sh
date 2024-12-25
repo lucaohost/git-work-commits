c69d976d875a78a982b6e9368336e8bfd62dfcdc
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-12-25 16:35:27 -0300

    feat: Add professional email check to git-work-commits script

--- git-work-commits.sh
+++ git-work-commits.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 filePath="/home/lucasreginatto/git/git-work-commits/git-work-commits-data.txt"
 
+if [ -z "$PROFESSIONAL_EMAIL" ]; then
+    echo "Professional email not provided. Exiting script."
+    exit 1
+fi
+
 # Iterar por cada repositório na pasta ~/git
 for repo in "$HOME/git"/*; do
     if [ -d "$repo/.git" ]; then
@@ -8,13 +13,13 @@ for repo in "$HOME/git"/*; do
         cd "$repo" || continue
 
         # Obter todas as branches locais criadas pelo autor
-        branches=$(git for-each-ref --format="%(refname:short) %(authorname) %(authoremail)" refs/heads/ | grep "lucas.reginatto@ifood.com.br" | awk '{print $1}')
+        branches=$(git for-each-ref --format="%(refname:short) %(authorname) %(authoremail)" refs/heads/ | grep $PROFESSIONAL_EMAIL | awk '{print $1}')
 
         for branch in $branches; do
             echo "  Processando branch: $branch"
             git checkout "$branch" &>/dev/null || continue
 
-            commits=$(git log --author="lucas.reginatto@ifood.com.br" --pretty=format:"%h|%ae|%ad|$(git symbolic-ref --short HEAD)|%s|$(basename $(git rev-parse --show-toplevel))" --abbrev=8 --date=iso)
+            commits=$(git log --author=$PROFESSIONAL_EMAIL --pretty=format:"%h|%ae|%ad|$(git symbolic-ref --short HEAD)|%s|$(basename $(git rev-parse --show-toplevel))" --abbrev=8 --date=iso)
             
             # Salvar os commits no arquivo
             echo "$commits" >> $filePath
